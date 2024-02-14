1aaf6e835983634b5c400cc0c1ee6ef6f4771e13
Jeddy <jeddy-awuor@users.noreply.github.com>
2024-02-14 10:33:32 +0300

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+
+        Dim frmObjectDemo As New Form2
+
+        frmObjectDemo.Show()
+        frmObjectDemo.Focus()
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim response As Integer
+
+        response = MessageBox.Show("Are you sure you want to exit?", "exit FirstApp",
+                                   MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+
+        If response = vbYes Then
+
+            Application.ExitThread()
+
+
+        End If
+
+    End Sub
+End Class
